d01db9ecb2b5824b2d6d7eef63af86fe0a75dc1d
raymondliu2022 <raymondliu2022@gmail.com>
2021-06-17 09:42:53 -0700

    Working better?

--- test4/app/src/main/java/com/example/test4/Book.kt
+++ test4/app/src/main/java/com/example/test4/Book.kt
@@ -2,113 +2,144 @@ package com.example.test4
 
 import android.content.Context
 import android.content.res.AssetManager
+import android.text.Html
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStreamReader
-import kotlin.system.measureTimeMillis
+import kotlin.math.min
 
-class Book(inFilePath: String, inAssetManager: AssetManager) {
+class Book(inFilePath: String, inAssetManager: AssetManager, inActivityContext: Context) {
     private val filePath: String = inFilePath
     private val assetManager: AssetManager = inAssetManager
-    lateinit var chapterStarts: ArrayList<Int>
-    private lateinit var paragraphStrings: ArrayList<String>
-    private var currentChapter = -1
+    var chapterStarts: ArrayList<Int> = ArrayList()
+    private val activityContext: Context = inActivityContext
 
-    fun indexChapters() {
-        try {
-            val bufferedReader = BufferedReader(InputStreamReader(assetManager.open(filePath)))
+    init {
+        val bufferedReader = BufferedReader(InputStreamReader(assetManager.open(filePath)))
 
-            chapterStarts = ArrayList()
-            var line = 0
-            var lineString: String? = ""
-            var emptyLineCount = 2
+        var line = 0
+        var lineString: String? = ""
+        var emptyLineCount = 2
 
-            val executionTime = measureTimeMillis {
-                do {
-                    if (lineString!!.isEmpty()) {
-                        emptyLineCount += 1
-                    } else {
-                        emptyLineCount = 0
-                    }
-                    if (emptyLineCount == 3) {
-                        chapterStarts.add(line)
-                    }
-                    line += 1
-                    lineString = bufferedReader.readLine()
-                } while (lineString != null)
+        do {
+            if (lineString!!.isEmpty()) {
+                emptyLineCount += 1
+            } else {
+                if (emptyLineCount >= 3) {
+                    chapterStarts.add(line)
+                }
+                emptyLineCount = 0
             }
-
-            Log.d("RL2022", "indexChapters() - ${chapterStarts.size} chapters found - ${line - 1} lines read - $executionTime milliseconds")
-        }
-        catch (e: IOException) {
-            Log.d("RL2022", "indexChapters() - IOException!")
-        }
+            line += 1
+            lineString = bufferedReader.readLine()
+        } while (lineString != null)
+        bufferedReader.close()
     }
 
-    fun loadChapterAtLine(line: Int) {
-        if (chapterStarts == null) {
-            Log.d("RL2022", "loadChapterAtLine( line $line ) - chapters haven't been indexed yet!")
-            return
-        }
+    lateinit var paragraphStrings: ArrayList<String>
+    var currentChapter = 0
+        set(inChapter) {
+            field = inChapter
 
-        var chapter = 0
-        for (chapterStart in chapterStarts) {
-            if (line < chapterStart) {break;}
-            chapter += 1
-        }
-        chapter -= 1
-
-        if (chapter >= chapterStarts.size) {
-            Log.d("RL2022", "loadChapterAtLine( line $line ) - lineIndex too large!")
-            return
-        }
-
-        if (chapter == currentChapter) {
-            Log.d("RL2022", "loadChapterAtLine( line $line ) - same chapter")
-            return
-        }
-
-        currentChapter = chapter
-        val chapterStart = chapterStarts[chapter]
-        val chapterEnd = chapterStarts[chapter + 1]
-
-        try {
             paragraphStrings = ArrayList()
 
-            val bufferedReader = BufferedReader(InputStreamReader(assetManager.open(filePath)))
+            if (field >= 0 && field < chapterStarts.size) {
+                val bufferedReader = BufferedReader(InputStreamReader(assetManager.open(filePath)))
 
-            for (line in 0..chapterStart) {
-                bufferedReader.readLine()
-            }
+                for (line in 0 until chapterStarts[field] - 1) {
+                    bufferedReader.readLine()
+                }
 
-            val executionTime = measureTimeMillis {
                 var paragraphBuffer = StringBuffer()
                 var lineBuffer = ""
-                for (line in chapterStart..chapterEnd) {
+                for (line in chapterStarts[field] until chapterStarts[field + 1]) {
                     lineBuffer = bufferedReader.readLine()
                     if (lineBuffer.isEmpty()) {
-                        paragraphStrings.add(paragraphBufferToString(paragraphBuffer))
-                        paragraphBuffer = StringBuffer()
+                        if (!paragraphBuffer.isEmpty()) {
+                            paragraphStrings.add(textStringHelper(paragraphBuffer.toString()))
+                            paragraphBuffer = StringBuffer()
+                        }
                     } else {
-                        paragraphBuffer.append(lineBuffer)
+                        paragraphBuffer.append("$lineBuffer ")
                     }
                 }
-                //paragraphStrings.add(paragraphBufferToString(paragraphBuffer))
+                bufferedReader.close()
             }
-
-            Log.d("RL2022", "loadChapterAtLine( line $line ) - ${paragraphStrings.size} paragraphs found - chapter $chapter - read lines $chapterStart to $chapterEnd - $executionTime milliseconds")
         }
-        catch (e: IOException) {
-            Log.d("RL2022", "loadChapterAtLine( line $line ) - IOException!")
+
+    var paragraphWidth = 0
+    var screenHeight = 0
+    var currentParagraph = 0
+
+    lateinit var underflowTextView: TextView
+    lateinit var underflowTextView2: TextView
+    lateinit var overflowTextView: TextView
+    lateinit var overflowTextView2: TextView
+
+    lateinit var pageViews: ArrayList<ArrayList<TextView>>
+    fun getPageView(index: Int): ArrayList<TextView> {
+        return if (index < pageViews.size && index >= 0) {
+            pageViews[index]
+        }
+        else {
+            ArrayList()
         }
     }
 
-    fun paragraphBufferToString(inBuffer: StringBuffer): String {
-        return "\t${inBuffer.toString().replace("\n","").replace("\t","")}" //Fix!
+    var currentPage: Int
+        get(){
+            var page = 0
+            var remainingParagraphs = currentParagraph
+            while (page < pageViews.size && pageViews[page].size <= remainingParagraphs ) {
+                remainingParagraphs -= pageViews[page].size
+                page += 1
+            }
+            Log.d("RL2022","get page $page paragraph $currentParagraph")
+            return page
+        }
+        set(inPage) {
+            currentParagraph = 0
+            for (page in 0 until min(inPage, pageViews.size)) {
+                currentParagraph += pageViews[page].size
+            }
+            Log.d("RL2022","set page $inPage paragraph $currentParagraph")
+        }
+
+    fun buildPages() {
+        pageViews = ArrayList()
+        pageViews.add(ArrayList())
+
+        var availableHeight = screenHeight
+        for (paragraphString in paragraphStrings) {
+            val textView =
+                View.inflate(activityContext, R.layout.paragraph_text_layout, null) as TextView
+            textView.text = Html.fromHtml(paragraphString)
+            textView.measure(
+                View.MeasureSpec.makeMeasureSpec(
+                    paragraphWidth,
+                    View.MeasureSpec.AT_MOST
+                ), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            )
+            if (textView.measuredHeight > availableHeight) {
+                pageViews.add(ArrayList())
+                availableHeight = screenHeight
+            }
+            availableHeight -= textView.measuredHeight
+            pageViews[pageViews.size - 1].add(textView)
+        }
+
+        underflowTextView = View.inflate(activityContext, R.layout.underflow_text_layout, null) as TextView
+        underflowTextView2 = View.inflate(activityContext, R.layout.underflow_text_layout, null) as TextView
+        overflowTextView = View.inflate(activityContext, R.layout.overflow_text_layout, null) as TextView
+        overflowTextView2 = View.inflate(activityContext, R.layout.overflow_text_layout, null) as TextView
     }
 
-    fun getParagraphStrings(): ArrayList<String> {
-        return paragraphStrings
+    private fun textStringHelper(inString: String): String {
+        var step1 = inString.replace("\\s+".toRegex(), " ")
+        var step2 = step1.replace("_(?<italics>((?!_).)+)_".toRegex(),"<i>\${italics}</i>")
+
+        return "\t$step2"
     }
 }
